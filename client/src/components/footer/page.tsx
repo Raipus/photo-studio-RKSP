27e3b07cc55313dad9d47e40700f65336691f301
Raipus <nikita18.907@gmail.com>
2024-09-19 18:44:14 +0300

    Header, Footer, Scrollbar
    Header, Footer, Scrollbar

---
+++ client/src/components/footer/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+const Footer = () => {
+    return (
+        <div className="row-start-3 flex absolute bottom-0 border-5 h-52 items-center justify-center bg-white p-10 min-w-full">
+            <p>Сайт был сделан солевыми наркоманами проходящими реабилитацию в подвале реабилитационного центра #1 Солнышко</p>
+        </div>
+    )
+}
+
+export default Footer;
