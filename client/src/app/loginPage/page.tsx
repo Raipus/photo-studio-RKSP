9b0a84f779699accfe12b0cd993023c002304893
Raipus <nikita18.907@gmail.com>
2024-10-14 15:45:41 +0300

    Login and Reg page design update
    Login and Reg page design update

--- client/src/app/loginPage/page.tsx
+++ client/src/app/loginPage/page.tsx
@@ -42,12 +42,12 @@ export default function LoginPage() {
       <Header/>
       <main className="grid justify-items-center">
         <div className="m-10 grid justify-items-center">
-          <h1 className="justify-self-center mb-3 mt-32">Вход</h1>
+          <h1 className="justify-self-center mb-3 mt-32 text-2xl">Вход</h1>
 
           <form onSubmit={handleSubmit(onSubmit)} className="justify-self-center grid space-y-4">
-            <input placeholder='Почта' type='email' {...register('email', { required: true })} />
-            <input placeholder='Пароль' type="password"{...register('password', {required: true})} />
-            <button  type="submit" className="bg-[#3D8361] rounded-md px-5 py-2 mt-20 text-xl hover: hover:scale-102 hover:bg-[#1C6758] duration-300">Вход</button>
+            <input className ="rounded-md text-black border-black border-[1px] p-1" placeholder='Почта' type='email' {...register('email', { required: true })} />
+            <input className ="rounded-md text-black border-black border-[1px] p-1" placeholder='Пароль' type="password"{...register('password', {required: true})} />
+            <button  type="submit" className="bg-[#3D8361] rounded-md px-5 py-2 mt-20 text-xl hover: hover:scale-102 hover:bg-[#2F6A4E] duration-300 text-white">Вход</button>
           </form>
         <Link href="http://localhost:3000/signPage" className="mt-3 mb-52">Регистрация</Link>
       </div>
--- server/dist/src/1726503191941-migrations.d.ts
+++
@@ -1,6 +0,0 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
-export declare class Migrations1726503191941 implements MigrationInterface {
-    name: string;
-    up(queryRunner: QueryRunner): Promise<void>;
-    down(queryRunner: QueryRunner): Promise<void>;
-}
