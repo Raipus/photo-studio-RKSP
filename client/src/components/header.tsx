bfc1c9b40dfb770a086fab5f7e67ec9c22397d19
Laptop Raipus <nikita18.907@gmail.com>
2024-10-19 23:49:29 +0300

    Header login/lk button logic

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -1,8 +1,24 @@
 'use client'
 
+import { getJwt } from '@/utils/auth/getJwt';
 import Link from 'next/link'
+import { useEffect, useState } from 'react';
 
 const Header = () => {
+    const [hasTokens, setHasTokens] = useState(false);
+
+    useEffect(() => {
+        async function TokenCheck (){
+            let refreshToken = (await getJwt()).refresh
+            if (!refreshToken) {
+                setHasTokens(false);
+            } else {
+                setHasTokens(true);
+            }
+        }
+        TokenCheck()
+    }, []);
+
     return (
         <div className="flex h-24 items-center justify-center dark:bg-[#111111] bg-[#EEF2E6] min-w-full transition-all duration-300 z-10">
             <div className="flex justify-between w-[1300px]">
@@ -16,10 +32,17 @@ const Header = () => {
                         <a className="p-4 mr-4">Фотографы</a>
                         <a className="p-4 mr-4">Контакты</a>
                     </div>
-                    <Link href='/signin'>
-                        <button className="pt-3 pb-3 pl-14 pr-14 bg-[#3D8361] hover:bg-[#2F6A4E] duration-300 rounded-md text-white">Вход
-                        </button>
-                    </Link>
+                    {hasTokens ? (
+                        <Link href='/adminID'>
+                            <button className="pt-3 pb-3 pl-12 pr-12 bg-[#3D8361] hover:bg-[#2F6A4E] duration-300 rounded-md text-white whitespace-nowrap">Личный кабинет
+                            </button>
+                        </Link>
+                    ) : (
+                        <Link href='/signin'>
+                            <button className="pt-3 pb-3 pl-14 pr-14 bg-[#3D8361] hover:bg-[#2F6A4E] duration-300 rounded-md text-white">Вход
+                            </button>
+                        </Link>
+                )}
                 </div>
             </div>
         </div>
